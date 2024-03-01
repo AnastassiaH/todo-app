c2aeeaa5efb7d6e42e08d79495a7dacdea2e60ca
Anastasiia Hurska <anastasiia.hurska@outlook.com>
2024-03-01 19:33:26 +0200

    clone todo app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config-react-typescript'],
+  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
   rules: {
     'max-len': ['error', {
       ignoreTemplateLiterals: true,
@@ -8,5 +8,6 @@ module.exports = {
     'jsx-a11y/label-has-associated-control': ["error", {
       assert: "either",
     }],
+    'operator-linebreak': 0,
   },
 };
